7afc4212b97cf35a5801b6918cee5957dfb9920f
Jaya-C23 <icm2015005@iiita.ac.in>
2017-01-18 19:41:04 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,6 @@
+import facebook
+import requests
+import flask
+app_id = 492888170881618
+
+flask.redirect("https://www.facebook.com/dialog/oauth?client_id=%s&redirect_uri=%s&scope=publish_actions % (FACEBOOK_APP_ID, REDIRECT_URI)")
